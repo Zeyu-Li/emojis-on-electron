c678728df302b4f275ecbd03dba03d6d4b00f91e
Zeyu-Li <zeyu.li23@gmail.com>
2021-01-09 22:00:50 -0600

    finished adding the small things

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 A "port" of my [Emoji Finder repo](https://github.com/Zeyu-Li/emoji-finder) in react
 
+![snapshot](img/snapshot.jpg)
+
 ## Created
 
 Created with `npx create-electron-app emojis-on-electron`
+
+## Licence
+
+MIT
--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 650,
+    minWidth: 350,
+    icon: 'src/img/favicon.png',
   });
 
   // and load the index.html of the app.
